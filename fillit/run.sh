eaf0032155c36e03902d112fed07427827ede104
sfisowilson <loverboy.sfiso@gmail.com>
2018-06-23 03:02:24 +0200

    deleting the swap files

--- fillit/run.sh
+++ fillit/run.sh
@@ -1,2 +1,3 @@
 gcc -Wall -Wextra -Werror -o sfiso.filler *.c ./libft/libft.a
-./resources/filler_vm -p1 ./sfiso.filler -p2 ./resources/players/hcao.filler  -v -f ./resources/maps/map02 >> log.txt
+rm log.txt
+./resources/filler_vm -p1 ./sfiso.filler -p2 ./sfiso.filler  -v -f ./resources/maps/map00 >> log.txt
